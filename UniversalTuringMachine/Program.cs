146af99ced958a7f9c580359a86145a940064794
nexus998 <mindaugasm.morkunas@gmail.com>
2019-09-22 19:26:06 +0300

    Added menu, custom functions

---
+++ UniversalTuringMachine/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalTuringMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize needed functions
+            var t = new TuringMachineFunction();
+
+
+
+            Program p = new Program();
+            p.DoWelcomeScreen();
+        }
+
+        private void DoWelcomeScreen()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("Welcome to a Turing machine simulator! Please select a function by entering the number of the function and pressing enter.");
+            Console.WriteLine("(made by Mindaugas Morkunas)");
+            Console.WriteLine();
+
+            LoadPrograms();
+            string input = Console.ReadLine();
+            ProgramFunction selectedFunction = ProgramSettings.GetProgramFunction(int.Parse(input)-1);
+            if(selectedFunction != null)
+            {
+                Console.Clear();
+                selectedFunction.RunProgram();
+                
+            }
+            else
+            {
+                Console.WriteLine("Command not found. Press any key to try again...");
+                Console.ReadKey();
+                DoWelcomeScreen();
+            }
+        }
+        void LoadPrograms()
+        {
+            for(int i = 0; i < ProgramSettings.GetProgramFunctions().Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + ProgramSettings.GetProgramFunctions()[i].GetName());
+            }
+        }
+    }
+}
---
+++ UniversalTuringMachine/TuringMachineFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalTuringMachine
+{
+    public class TuringMachineFunction
+    {
+        public TuringMachineFunction()
+        {
+            var f = new ProgramFunction("Load Turing Machine Files", RunProgram);
+        }
+
+        List<string> fileNames = new List<string>();
+
+        void RunProgram()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Files selected: " + fileNames.Count);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Enter the command \"RUN\" to begin the simulations");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+            Console.WriteLine("Enter file name of code and press enter:");
+            string name = Console.ReadLine();
+
+        }
+    }
+}
